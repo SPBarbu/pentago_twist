f1e040ce653b05dc1b4c5473aacfa53686f614f4
SPBarbu <stefpb0@live.ca>
2021-04-08 23:05:51 -0400

    detecting opponent's move

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -1,7 +1,47 @@
 package student_player;
 
+import pentago_twist.PentagoBoardState.Piece;
+
 public class MyTools {
-    public static double getSomething() {
-        return Math.random();
+    private static final int QUAD_SIZE = 3;
+
+    public static String get_coord_last_move(Piece[][] last_m, Piece[][] new_m) {
+        if (get_number_pieces(last_m) == get_number_pieces(new_m)) {
+            return "";
+        } else {
+
+        }
+        return "";
+    }
+
+    // assumes move did happen, ie. different number of pieces
+    public static int move_is_rotate(Piece[][] last_m, Piece[][] new_m, int new_num_pieces) {
+        int n = 0;
+        Piece[][] temp = new Piece[QUAD_SIZE][QUAD_SIZE];
+        for (int j = 0; j < QUAD_SIZE; j++) {
+            for (int i = QUAD_SIZE - 1; i >= 0; i--) {
+                System.arraycopy(last_m[i], j, temp[j], QUAD_SIZE - 1 - i, 1);
+                if(){
+
+                }
+            }
+        }
+    }
+
+    // assumes move did happen, ie. different number of pieces
+    public static boolean move_is_flip(Piece[][] last_m, Piece[][] new_m, int new_num_pieces) {
+
+    }
+
+    public static int get_number_pieces(Piece[][] q) {
+        int n = 0;
+        for (int i = 0; i < QUAD_SIZE; i++) {
+            for (int j = 0; j < QUAD_SIZE; j++) {
+                if (q[i][j] != Piece.EMPTY) {
+                    n += 1;
+                }
+            }
+        }
+        return n;
     }
 }
