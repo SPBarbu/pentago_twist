d85a56e9a04471484bf3cc0fa8649999ab6599ae
SPBarbu <stefpb0@live.ca>
2021-04-13 00:06:28 -0400

    2nd player logic

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -3,19 +3,68 @@ package student_player;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import java.util.concurrent.TimeUnit;
+
 import pentago_twist.PentagoBoardState.Piece;
+import pentago_twist.PentagoBoardState;
+import pentago_twist.PentagoMove;
+import boardgame.Move;
 
 public class MyTools {
-    private static final int QUAD_SIZE = 3;
+    public static final int QUAD_SIZE = 3;
+    public static final int NUM_QUADS = 4;
     private static Piece[][] quad_1 = new Piece[][] { {}, {}, {} };
     private static ArrayList<String> all_moves = new ArrayList<String>(
             Arrays.asList("00", "01", "02", "10", "11", "12", "20", "21", "22"));
-    private static int[] quad_strat = new int[] { 0, 0, 0, 0 };
+    public static PentagoBoardState board_state_after_my_last_move;
+    public static PentagoBoardState board_state_opp_move;
+    public static int[] quad_strat = new int[] { 0, 0, 0, 0 };
+    public static int[] quad_turns = new int[] { 0, 0, 0, 0 };
     // 0:continue best
     // 1:continue for win
     // 2:continue for draw
     // 3:won
     // 4:drawn
+
+    public static int get_quadrant_updated() {
+        if (board_state_opp_move.getTurnNumber() == 0) {
+            if (board_state_opp_move.getTurnPlayer() == 0) {
+                return -1;
+            } else {
+                for (int i = 0; i < NUM_QUADS; i++) {
+                    if (get_number_pieces(get_quadrant_from_board(board_state_opp_move.getBoard(), i)) == 1) {
+                        return i;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < NUM_QUADS; i++) {
+            if (get_number_pieces(get_quadrant_from_board(board_state_after_my_last_move.getBoard(),
+                    i)) != get_number_pieces(get_quadrant_from_board(board_state_opp_move.getBoard(), i))) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static Piece[][] get_quadrant_from_board(Piece[][] board, int quad_numb) {
+        Piece[][] q = new Piece[QUAD_SIZE][QUAD_SIZE];
+        for (int i = (int) (Math.floor(quad_numb / 2) * 3); i < (int) (Math.floor(quad_numb / 2) * 3) + 3; i++) {
+            for (int j = (quad_numb % 2) * 3; j < (quad_numb % 2) * 3 + 3; j++) {
+                q[i - (int) (Math.floor(quad_numb / 2) * 3)][j - (quad_numb % 2) * 3] = board[i][j];
+            }
+        }
+        return q;
+    }
+
+    public static Move string_to_move(String move, int quad_numb, int playerId) {
+        if (move == "") {
+            return board_state_opp_move.getRandomMove();
+        }
+        Move m = new PentagoMove((int) (Math.floor(quad_numb / 2) * 3) + (move.charAt(0) - '0'),
+                (quad_numb % 2) * 3 + (move.charAt(1) - '0'), 0, 0, playerId);
+        return m;
+    }
 
     public static String get_best_move(Piece[][] q, int q_number, int my_turn, Piece my_piece) {
         int n_pieces_new = get_number_pieces(q);
@@ -30,6 +79,10 @@ public class MyTools {
                         return "22";
                     } else if (q[0][2] == my_piece) {
                         return "20";
+                    } else if (q[2][0] == my_piece) {
+                        return "02";
+                    } else {
+                        return "00";
                     }
                 } else {// not center play
                     if (q[0][0] == my_piece) {
@@ -54,6 +107,30 @@ public class MyTools {
                                 return "22";
                             } else {
                                 return "00";
+                            }
+                        }
+                    } else if (q[2][0] == my_piece) {
+                        if (q[1][0] == en_piece) {
+                            return "22";
+                        } else if (q[2][1] == en_piece) {
+                            return "00";
+                        } else {
+                            if (q[2][2] == Piece.EMPTY) {
+                                return "22";
+                            } else {
+                                return "00";
+                            }
+                        }
+                    } else {
+                        if (q[2][1] == en_piece) {
+                            return "02";
+                        } else if (q[1][2] == en_piece) {
+                            return "20";
+                        } else {
+                            if (q[2][0] == Piece.EMPTY) {
+                                return "20";
+                            } else {
+                                return "02";
                             }
                         }
                     }
@@ -188,6 +265,50 @@ public class MyTools {
                                         return "00";
                                     } else {
                                         return "20";
+                                    }
+                                }
+                            }
+                        }
+                    } else {// bottom left
+                        if (q[1][0] == en_piece) {// touch left
+                            if (q[2][1] == Piece.EMPTY) {
+                                quad_strat[q_number] = 3;
+                                return "21";
+                            } else {
+                                quad_strat[q_number] = 1;
+                                return "02";
+                            }
+                        } else if (q[2][1] == en_piece) {// touch right
+                            if (q[1][0] == Piece.EMPTY) {
+                                quad_strat[q_number] = 3;
+                                return "10";
+                            } else {
+                                quad_strat[q_number] = 1;
+                                return "02";
+                            }
+                        } else {
+                            if (q[0][0] == my_piece) {
+                                if (q[1][0] == Piece.EMPTY) {
+                                    quad_strat[q_number] = 3;
+                                    return "10";
+                                } else {
+                                    quad_strat[q_number] = 1;
+                                    if (q[2][2] == Piece.EMPTY) {
+                                        return "22";
+                                    } else {
+                                        return "02";
+                                    }
+                                }
+                            } else {
+                                if (q[2][1] == Piece.EMPTY) {
+                                    quad_strat[q_number] = 3;
+                                    return "21";
+                                } else {
+                                    quad_strat[q_number] = 1;
+                                    if (q[0][0] == Piece.EMPTY) {
+                                        return "00";
+                                    } else {
+                                        return "02";
                                     }
                                 }
                             }
@@ -367,16 +488,42 @@ public class MyTools {
     }
 
     public static void main(String[] args) {
-        Piece[][] a = process_move_quadrant(new Piece[][] { { Piece.WHITE, Piece.EMPTY, Piece.EMPTY },
-                { Piece.EMPTY, Piece.EMPTY, Piece.EMPTY }, { Piece.EMPTY, Piece.EMPTY, Piece.EMPTY } }, "12",
-                Piece.BLACK);
-        System.out.println(toString(a));
-        ArrayList<String> c = get_legal_moves_quadrant(a);
-        ArrayList<Piece[][]> d = new ArrayList<Piece[][]>();
-        for (int i = 0; i < c.size(); i++) {
-            d.add(process_move_quadrant(a, c.get(i), Piece.WHITE));
+        long startTime = System.nanoTime();
+        Piece[][] b = new Piece[][] { { Piece.EMPTY, Piece.EMPTY, Piece.EMPTY },
+                { Piece.EMPTY, Piece.BLACK, Piece.EMPTY }, { Piece.BLACK, Piece.EMPTY, Piece.EMPTY } };
+
+        System.out.println(continue_for_draw(b, 0, Piece.WHITE));
+        long endTime = System.nanoTime();
+        System.out.println(endTime - startTime);
+    }
+
+    public static String continue_for_win(Piece[][] q, int q_number, Piece my_piece) {
+        for (String move : get_legal_moves_quadrant(q)) {
+            if (get_longest(process_move_quadrant(q, move, my_piece), my_piece) == 3) {
+                quad_strat[q_number] = 3;
+                return move;
+            }
         }
-        int b = 0;
+        return "";
+    }
+
+    public static String continue_for_draw(Piece[][] q, int q_number, Piece my_piece) {
+        Piece en_piece = my_piece == Piece.WHITE ? Piece.BLACK : Piece.WHITE;
+        if (get_number_pieces(q) == 8) {
+            quad_strat[q_number] = 4;
+        }
+        String instant_win_possibility = continue_for_win(q, q_number, my_piece);
+        if (instant_win_possibility == "") {
+            for (String move : get_legal_moves_quadrant(q)) {
+                if (get_longest(process_move_quadrant(q, move, en_piece), en_piece) == 3) {
+                    return move;
+                }
+            }
+        } else {
+            quad_strat[q_number] = 4;
+            return instant_win_possibility;
+        }
+        return get_random_move(q);
     }
 
     public static String get_last_move(Piece[][] last_quad, Piece[][] new_quad) {
@@ -479,14 +626,11 @@ public class MyTools {
         }
 
         // longest vertical
-        Piece[] rot;
+        Piece[] rot = new Piece[QUAD_SIZE];
         for (int j = 0; j < QUAD_SIZE; j++) {
-            rot = new Piece[QUAD_SIZE];
-            for (int i = 0; i < QUAD_SIZE; i++) {
-                rot[i] = quadrant[i][0];
-                rot[i] = quadrant[i][0];
-                rot[i] = quadrant[i][0];
-            }
+            rot[0] = quadrant[0][j];
+            rot[1] = quadrant[1][j];
+            rot[2] = quadrant[2][j];
             t = get_longest_line(rot, player);
             if (t > l) {
                 l = t;
@@ -549,5 +693,16 @@ public class MyTools {
             }
         }
         return n;
+    }
+
+    public static String get_random_move(Piece[][] q) {
+        for (int i = 0; i < QUAD_SIZE; i++) {
+            for (int j = 0; j < QUAD_SIZE; j++) {
+                if (q[i][j] == Piece.EMPTY) {
+                    return i + "" + j;
+                }
+            }
+        }
+        return "";
     }
 }
--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -3,7 +3,9 @@ package student_player;
 import boardgame.Move;
 
 import pentago_twist.PentagoPlayer;
+import pentago_twist.PentagoBoardState.Piece;
 import pentago_twist.PentagoBoardState;
+import pentago_twist.PentagoMove;
 
 /** A player file submitted by a student. */
 public class StudentPlayer extends PentagoPlayer {
@@ -23,11 +25,57 @@ public class StudentPlayer extends PentagoPlayer {
      * make decisions.
      */
     public Move chooseMove(PentagoBoardState boardState) {
+        long startTime = System.nanoTime();
+        // update opponent's move
+        MyTools.board_state_opp_move = (PentagoBoardState) boardState.clone();
 
-        // Is random the best you can do?
-        Move myMove = boardState.getRandomMove();
+        Piece my_piece = Piece.BLACK;
+        int quad_updated = MyTools.get_quadrant_updated();
+        int q_strat = MyTools.quad_strat[quad_updated];
+        String b_m = "";
+        Piece[][] play_q = MyTools.get_quadrant_from_board(MyTools.board_state_opp_move.getBoard(), quad_updated);
+        boolean play_q_isfull = MyTools.get_number_pieces(play_q) == 9;
+        Move myMove;
+
+        if (q_strat == 0) {
+            if (!play_q_isfull) {
+                b_m = MyTools.get_best_move(play_q, quad_updated, 1, my_piece);
+                myMove = MyTools.string_to_move(b_m, quad_updated, this.player_id);
+            } else {// play on new board when quad is full
+                quad_updated = (quad_updated + 1) % 4;
+                play_q = MyTools.get_quadrant_from_board(MyTools.board_state_opp_move.getBoard(), quad_updated);
+                b_m = MyTools.get_best_move(
+                        MyTools.get_quadrant_from_board(MyTools.board_state_opp_move.getBoard(), quad_updated),
+                        quad_updated, 0, my_piece);
+                myMove = MyTools.string_to_move(b_m, quad_updated, this.player_id);
+            }
+        } else if (q_strat == 1) {
+            b_m = MyTools.continue_for_win(play_q, quad_updated, my_piece);
+            myMove = MyTools.string_to_move(b_m, quad_updated, this.player_id);
+        } else if (q_strat == 2) {
+            if (play_q_isfull) {
+                MyTools.quad_strat[quad_updated] = 4;
+                quad_updated = (quad_updated + 1) % 4;
+                play_q = MyTools.get_quadrant_from_board(MyTools.board_state_opp_move.getBoard(), quad_updated);
+                b_m = MyTools.get_best_move(
+                        MyTools.get_quadrant_from_board(MyTools.board_state_opp_move.getBoard(), quad_updated),
+                        quad_updated, 0, my_piece);
+                myMove = MyTools.string_to_move(b_m, quad_updated, this.player_id);
+            } else {
+                b_m = MyTools.continue_for_draw(play_q, quad_updated, my_piece);
+                myMove = MyTools.string_to_move(b_m, quad_updated, this.player_id);
+            }
+        } else {
+            myMove = MyTools.board_state_opp_move.getRandomMove();
+        }
+
+        // save my last move
+        MyTools.board_state_after_my_last_move = (PentagoBoardState) MyTools.board_state_opp_move.clone();
+        MyTools.board_state_after_my_last_move.processMove((PentagoMove) myMove);
 
         // Return your move to be processed by the server.
+        long endTime = System.nanoTime();
+        System.out.println(endTime - startTime);
         return myMove;
     }
 }
